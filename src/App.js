0a5b6421c3e8d6ecea2524c6968f331d6d78cca8
Grzegorz Szermiński <szerman@poczta.fm>
2019-07-15 09:06:53 +0200

    add NavLink to navigation and styles

--- src/App.js
+++ src/App.js
@@ -1,25 +1,48 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import {
+  BrowserRouter as Router,
+  Link,
+  NavLink,
+  Route
+} from "react-router-dom";
+
+const Home = () => <h1>Strona startowa</h1>;
+const News = () => <h2>Aktualności</h2>;
+const Contact = () => <h2>Kontakt do nas</h2>;
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div>
+        <header>
+          <nav>
+            <ul>
+              <li>
+                <NavLink exact to="/" activeClassName="home_selected">
+                  Start
+                </NavLink>
+              </li>
+              <li>
+                <NavLink to="/news" activeClassName="news_selected">
+                  Aktualności
+                </NavLink>
+              </li>
+              <li>
+                <NavLink to="/contact" activeClassName="contact_selected">
+                  Kontakt
+                </NavLink>
+              </li>
+            </ul>
+          </nav>
+        </header>
+        <section>
+          <Route path="/" exact component={Home} />
+          <Route path="/news" component={News} />
+          <Route path="/contact" component={Contact} />
+        </section>
+      </div>
+    </Router>
   );
 }
 
